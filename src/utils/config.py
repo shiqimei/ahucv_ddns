59b3af89889fa81c792d7728bbc9f18ff4dc7eee
lolimay <1404363070@qq.com>
2019-09-05 20:14:10 +0800

    rename load_configrations to config

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,16 +1,16 @@
-"""
+'''
 Load Configrations from ini file
-@params path : String
-@returns username : String, password : String
-"""
+'''
 
-import os
 import configparser
 
 def load_configrations(path, key='DEFAULT'):
+    '''
+    load configrations by specifying config file path and the section
+    '''
     parser = configparser.ConfigParser()
     parser.read(path, encoding='utf-8')
-    
+
     items = parser.items(key)
 
     return items[0][1], items[1][1]
