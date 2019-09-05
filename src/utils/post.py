ab9c81e71e6623efc14004f6843a30e71e00fc34
lolimay <1404363070@qq.com>
2019-09-05 19:55:54 +0800

    update docstring for post.py

--- src/utils/post.py
+++ src/utils/post.py
@@ -1,10 +1,13 @@
-"""
-HTTP Post
-"""
+'''
+a simple HTTP Post method
+'''
 
 import requests
 
-def post(url, data, headers):
+def post(url, data, headers=None):
+    '''
+    Post data by sepcifying target url, payload data and headers
+    '''
     session = requests.Session()
     session.keep_alive = False
     res = session.post(url, data=data, headers=headers)
