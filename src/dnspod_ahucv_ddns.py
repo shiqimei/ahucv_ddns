9819e1856df9b6217f41d878567894b6a3a9e3f0
lolimay <lolimay@lolimay.cn>
2019-09-21 14:18:53 +0800

    chore: optimize code structure

--- src/dnspod_ahucv_ddns.py
+++ src/dnspod_ahucv_ddns.py
@@ -8,19 +8,18 @@ import requests
 from utils import config, DrCOM, DnsPod
 
 APP_VERSION = '1.0.0'
-CONFIG_PATH = './config.ini'
+CONFIG_PATH = 'config.ini'
 
 username, password = config.load(CONFIG_PATH, 'AUTH')
 dnspod_id, dnspod_token, author_email = config.load(CONFIG_PATH, 'DNSPOD')
 sub_domain, domain = config.load(CONFIG_PATH, 'DOMAIN')
 mac_address, *_ = config.load(CONFIG_PATH, 'CLIENT')
 
-dnspod_client = DnsPod.DnsPodClient(dnspod_id, dnspod_token, author_email, APP_VERSION)
-
 need_update = False
 update_result = ''
 
 new_ip = DrCOM.get_ip_by_mac_address(username, password, mac_address)
+dnspod_client = DnsPod.DnsPodClient(dnspod_id, dnspod_token, author_email, APP_VERSION)
 record_id, old_ip = dnspod_client.get_record(sub_domain, domain)
 
 if new_ip and old_ip and new_ip != old_ip:
